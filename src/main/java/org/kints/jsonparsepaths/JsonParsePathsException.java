86c610282afd5ada409879874cb0eeeaf48d7088
makintsian <howletttest@gmail.com>
2018-06-01 00:07:27 +0300

    Add 'JsonParsePaths'

---
+++ src/main/java/org/kints/jsonparsepaths/JsonParsePathsException.java
@@ -0,0 +1,11 @@
+package org.kints.jsonparsepaths;
+
+public class JsonParsePathsException extends RuntimeException {
+
+    public JsonParsePathsException() {
+    }
+
+    public JsonParsePathsException(String message) {
+        super(message);
+    }
+}
---
+++ src/test/java/org/kints/jsonparsepaths/JsonParsePathTest.java
@@ -0,0 +1,60 @@
+package org.kints.jsonparsepaths;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class JsonParsePathTest {
+
+    private String json = "{\n" +
+            "  \"firstName\": \"John\",\n" +
+            "  \"lastName\" : \"doe\",\n" +
+            "  \"age\"      : 26,\n" +
+            "  \"address\"  : {\n" +
+            "    \"streetAddress\": \"naist street\",\n" +
+            "    \"city\"         : \"Nara\",\n" +
+            "    \"postalCode\"   : \"630-0192\"\n" +
+            "  },\n" +
+            "  \"phoneNumbers\": [\n" +
+            "    {\n" +
+            "      \"type\"  : \"iPhone\",\n" +
+            "      \"number\": \"0123-4567-8888\"\n" +
+            "    },\n" +
+            "    {\n" +
+            "      \"type\"  : \"home\",\n" +
+            "      \"number\": \"0123-4567-8910\"\n" +
+            "    }\n" +
+            "  ]\n" +
+            "}";
+
+
+    @Test
+    public void testGetJsonPathsList(){
+        JsonParsePaths jsonParsePaths = new JsonParsePaths(json);
+        List<String> actual = jsonParsePaths.getJsonPathsList();
+        List<String> expected = Arrays.asList("address.city", "address.postalCode", "address.streetAddress", "age",
+                "firstName", "lastName", "phoneNumbers[*].number", "phoneNumbers[*].type");
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetJsonPathsStr(){
+        JsonParsePaths jsonParsePaths = new JsonParsePaths(json);
+        String actual = jsonParsePaths.getJsonPathsStr();
+        String expected = "address.city, address.postalCode, address.streetAddress, age, firstName, lastName, " +
+                "phoneNumbers[*].number, phoneNumbers[*].type";
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetJsonFullPathsList(){
+        JsonParsePaths jsonParsePaths = new JsonParsePaths(json);
+        List<String> actual = jsonParsePaths.getJsonFullPathsList();
+        List<String> expected = Arrays.asList("address.city", "address.postalCode", "address.streetAddress", "age",
+                "firstName", "lastName", "phoneNumbers[0].number", "phoneNumbers[0].type", "phoneNumbers[1].number",
+                "phoneNumbers[1].type");
+        Assert.assertEquals(expected, actual);
+    }
+}
